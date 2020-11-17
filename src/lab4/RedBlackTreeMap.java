3f885108a6c54d27ae3f9500349020c1cf26f816
Julian Saldana <julianesaldana@yahoo.com>
2020-11-16 23:28:30 -0800

    FINISHED

--- src/lab4/RedBlackTreeMap.java
+++ src/lab4/RedBlackTreeMap.java
@@ -5,261 +5,248 @@ package lab4;
 // Comparable.
 // The key type of a map must be Comparable. Values can be any type.
 public class RedBlackTreeMap<TKey extends Comparable<TKey>, TValue> {
-	// A Node class.
-	private class Node {
-		private TKey mKey;
-		private TValue mValue;
-		private Node mParent;
-		private Node mLeft;
-		private Node mRight;
-		private boolean mIsRed;
+    // A Node class.
+    private class Node {
+        private TKey mKey;
+        private TValue mValue;
+        private Node mParent;
+        private Node mLeft;
+        private Node mRight;
+        private boolean mIsRed;
 
-		// Constructs a new node with NIL children.
-		public Node(TKey key, TValue data, boolean isRed) {
-			mKey = key;
-			mValue = data;
-			mIsRed = isRed;
+        // Constructs a new node with NIL children.
+        public Node(TKey key, TValue data, boolean isRed) {
+            mKey = key;
+            mValue = data;
+            mIsRed = isRed;
 
-			mLeft = NIL_NODE;
-			mRight = NIL_NODE;
-		}
+            mLeft = NIL_NODE;
+            mRight = NIL_NODE;
+        }
 
-		@Override
-		public String toString() {
-			return mKey + " : " + mValue + " (" + (mIsRed ? "red)" : "black)");
-		}
-	}
+        @Override
+        public String toString() {
+            return mKey + " : " + mValue + " (" + (mIsRed ? "red)" : "black)");
+        }
+    }
 
-	private Node mRoot;
-	private int mCount;
+    private Node mRoot;
+    private int mCount;
 
-	// Rather than create a "blank" black Node for each NIL, we use one shared node for all NIL leaves
-	private final Node NIL_NODE = new Node(null, null, false);
+    // Rather than create a "blank" black Node for each NIL, we use one shared node for all NIL leaves
+    private final Node NIL_NODE = new Node(null, null, false);
 
-	//////////////////// I give you these utility functions for free.
+    //////////////////// I give you these utility functions for free.
 
-	// Get the # of keys in the tree.
-	public int getCount() {
-		return mCount;
-	}
+    // Get the # of keys in the tree.
+    public int getCount() {
+        return mCount;
+    }
 
-	// Finds the value associated with the given key.
-	public TValue find(TKey key) {
-		Node n = bstFind(key, mRoot); // find the Node containing the key if any
-		if (n == null || n == NIL_NODE)
-			throw new RuntimeException("Key not found");
-		return n.mValue;
-	}
+    // Finds the value associated with the given key.
+    public TValue find(TKey key) {
+        Node n = bstFind(key, mRoot); // find the Node containing the key if any
+        if (n == null || n == NIL_NODE)
+            throw new RuntimeException("Key not found");
+        return n.mValue;
+    }
 
-	/////////////////// You must finish the rest of these methods.
+    /////////////////// You must finish the rest of these methods.
 
-	// Inserts a key/value pair into the tree, updating the red/black balance of nodes as necessary. Starts with a
-	// normal BST insert, then adjusts.
-	public void add(TKey key, TValue data) {
-		Node n = new Node(key, data, true); // nodes start red
+    // Inserts a key/value pair into the tree, updating the red/black balance of nodes as necessary. Starts with a
+    // normal BST insert, then adjusts.
+    public void add(TKey key, TValue data) {
+        Node n = new Node(key, data, true); // nodes start red
 
-		// normal BST insert; n will be placed into its initial position.
-		// returns false if an existing node was updated (no rebalancing needed)
-		boolean insertedNew = bstInsert(n, mRoot);
-		if (!insertedNew)
-			return;
+        // normal BST insert; n will be placed into its initial position.
+        // returns false if an existing node was updated (no rebalancing needed)
+        boolean insertedNew = bstInsert(n, mRoot);
+        if (!insertedNew)
+            return;
 
-		// check cases 1-5 for balance violations.
-		checkBalance(n);
-	}
+        // check cases 1-5 for balance violations.
+        checkBalance(n);
+    }
 
-	// Applies rules 1-5 to check the balance of a tree with newly inserted node n.
-	private void checkBalance(Node n) {
-		// case 1: new node is root.
-		if (n == mRoot) {
-			n.mIsRed = false;
-			return;
-		}
+    // Applies rules 1-5 to check the balance of a tree with newly inserted node n.
+    private void checkBalance(Node n) {
+        // case 1: new node is root.
+        if (n == mRoot) {
+            n.mIsRed = false;
+            return;
+        }
 
-		// case 2: parent is black
-		if (n.mParent.mIsRed){
-			// case 3: parent, child, uncle are all red. recolor parent and uncle to black and grandparent to red
-			if (getUncle(n) != null && getUncle(n).mIsRed) {
-				n.mParent.mIsRed = false;
-				getUncle(n).mIsRed = false;
-				getGrandparent(n).mIsRed = true;
-				checkBalance(getGrandparent(n));
-			}
-			else{
-				// case 4, parent is red and uncle is black, node must be LR or RL
-				Node parent = n.mParent;
-				Node grandparent = getGrandparent(n);
+        // case 2: parent is black
+        if (n.mParent.mIsRed) {
+            // case 3: parent, child, uncle are all red. recolor parent and uncle to black and grandparent to red
+            if (getUncle(n) != null && getUncle(n).mIsRed) {
+                n.mParent.mIsRed = false;
+                getUncle(n).mIsRed = false;
+                getGrandparent(n).mIsRed = true;
+                checkBalance(getGrandparent(n));
+            } else {
+                // case 4, parent is red and uncle is black, node must be LR or RL
+                Node parent = n.mParent;
+                Node grandparent = getGrandparent(n);
 
-				if (n == parent.mRight && parent == grandparent.mLeft){
-					singleRotateLeft(parent);
-					n = n.mLeft;
-				}
-				else if (n == parent.mLeft && parent == grandparent.mRight){
-					singleRotateRight(parent);
-					n = n.mRight;
-				}
+                if (n == parent.mRight && parent == grandparent.mLeft) {
+                    singleRotateLeft(parent);
+                    n = n.mLeft;
+                } else if (n == parent.mLeft && parent == grandparent.mRight) {
+                    singleRotateRight(parent);
+                    n = n.mRight;
+                }
 
-				// case 5, setup usually from case 4 but can occur without case 4, node can be LL or anything else other than LR/RL
-				parent = n.mParent;
-				grandparent = getGrandparent(n);
-				if (n == parent.mLeft && grandparent.mLeft == parent)
-					singleRotateRight(grandparent);
-				else
-					singleRotateLeft(grandparent);
+                // case 5, setup usually from case 4 but can occur without case 4, node can be LL or anything else other than LR/RL
+                parent = n.mParent;
+                grandparent = getGrandparent(n);
+                if (n == parent.mLeft && grandparent.mLeft == parent)
+                    singleRotateRight(grandparent);
+                else
+                    singleRotateLeft(grandparent);
 
-				parent.mIsRed = false;
-				grandparent.mIsRed = true;
-			}
-		}
-	}
+                parent.mIsRed = false;
+                grandparent.mIsRed = true;
+            }
+        }
+    }
 
-	// Returns true if the given key is in the tree.
-	public boolean containsKey(TKey key) {
-		Node n = bstFind(key, mRoot); // find the Node containing the key if any
-		if (n == null || n == NIL_NODE)
-			return false;
-		return true;
-	}
+    // Returns true if the given key is in the tree.
+    public boolean containsKey(TKey key) {
+        Node n = bstFind(key, mRoot); // find the Node containing the key if any
+        if (n == null || n == NIL_NODE)
+            return false;
+        return true;
+    }
 
-	// Prints a pre-order traversal of the tree's nodes, printing the key, value, and color of each node.
-	public void printStructure(Node n) {
-		if (n != NIL_NODE) {
-			System.out.println(n.toString());
-			printStructure(n.mLeft);
-			printStructure(n.mRight);
-		}
-	}
+    // Prints a pre-order traversal of the tree's nodes, printing the key, value, and color of each node.
+    public void printStructure(Node n) {
+        if (n != NIL_NODE) {
+            System.out.println(n.toString());
+            printStructure(n.mLeft);
+            printStructure(n.mRight);
+        }
+    }
 
-	// Returns the Node containing the given key. Recursive.
-	private Node bstFind(TKey key, Node currentNode) {
-		// Given a key to find and a node to start at,
-		// proceed left/right from the current node until finding a node whose
-		// key is equal to the given key.
-		// Base Cases: root is null or key is present at root
-		if (currentNode==null || currentNode.mKey == key)
-			return currentNode;
+    // Returns the Node containing the given key. Recursive.
+    private Node bstFind(TKey key, Node currentNode) {
+        // Given a key to find and a node to start at, proceed left/right from the current node until finding a node whose key is equal to the given key.
 
-		// Key is greater than node key
-		if (currentNode.mKey.compareTo(key) > 0)
-			return bstFind(key, currentNode.mLeft);
-		// Key is smaller than node key
-		else if (currentNode.mKey.compareTo(key) < 0)
-			return bstFind(key, currentNode.mRight);
-		// key is the same as the node key
-		else if (currentNode.mKey.compareTo(key) == 0)
-			return currentNode;
-		else
-			return null;
-	}
+        // Base Cases: root is null or key is present at root
+        if (currentNode == null || currentNode.mKey == key)
+            return currentNode;
+        // Key is greater than node key
+        if (currentNode.mKey.compareTo(key) > 0)
+            return bstFind(key, currentNode.mLeft);
+        // Key is smaller than node key
+        else if (currentNode.mKey.compareTo(key) < 0)
+            return bstFind(key, currentNode.mRight);
+        // key is the same as the node key
+        else if (currentNode.mKey.compareTo(key) == 0)
+            return currentNode;
+        // key is not found
+        else
+            return null;
+    }
 
 
+    //////////////// These functions are needed for insertion cases.
 
-	//////////////// These functions are needed for insertion cases.
+    // Gets the grandparent of n.
+    private Node getGrandparent(Node n) {
+        if (n.mParent.mParent != null)
+            return n.mParent.mParent;
+        return null;
+    }
 
-	// Gets the grandparent of n.
-	private Node getGrandparent(Node n) {
-		if (n.mParent.mParent != null)
-			return n.mParent.mParent;
-		return null;
-	}
+    // Gets the uncle (parent's sibling) of n.
+    private Node getUncle(Node n) {
+        Node grandParent = getGrandparent(n);
+        assert grandParent != null;
+        if (grandParent.mLeft == n.mParent)
+            return grandParent.mRight;
+        else if (grandParent.mRight == n.mParent)
+            return grandParent.mLeft;
+        return null;
+    }
 
-	// Gets the uncle (parent's sibling) of n.
-	private Node getUncle(Node n) {
-		Node grandParent = getGrandparent(n);
-		assert grandParent != null;
-		if (grandParent.mLeft == n.mParent)
-			return grandParent.mRight;
-		else if (grandParent.mRight == n.mParent)
-			return grandParent.mLeft;
-		return null;
-	}
+    // Rotate the tree right at the given node.
+    private void singleRotateRight(Node n) {
+        Node l = n.mLeft, lr = l.mRight, p = n.mParent;
+        n.mLeft = lr;
+        lr.mParent = n;
+        l.mRight = n;
+        n.mParent = l;
 
-	// Rotate the tree right at the given node.
-	private void singleRotateRight(Node n) {
-		Node l = n.mLeft, lr = l.mRight, p = n.mParent;
-		n.mLeft = lr;
-		lr.mParent = n;
-		l.mRight = n;
-		n.mParent = l;
+        if (p == null) { // n is root
+            mRoot = l;
+        } else if (p.mLeft == n) {
+            p.mLeft = l;
+        } else {
+            p.mRight = l;
+        }
 
-		if (p == null) { // n is root
-			mRoot = l;
-		}
-		else if (p.mLeft == n) {
-			p.mLeft = l;
-		}
-		else {
-			p.mRight = l;
-		}
+        l.mParent = p;
+    }
 
-		l.mParent = p;
-	}
+    // Rotate the tree left at the given node.
+    private void singleRotateLeft(Node n) {
+        Node r = n.mRight, rl = r.mLeft, p = n.mParent;
+        n.mRight = rl;
+        rl.mParent = n;
+        r.mLeft = n;
+        n.mParent = r;
 
-	// Rotate the tree left at the given node.
-	private void singleRotateLeft(Node n) {
-		Node r = n.mRight, rl = r.mLeft, p = n.mParent;
-		n.mRight = rl;
-		rl.mParent = n;
-		r.mLeft = n;
-		n.mParent = r;
+        if (p == null) { // n is root
+            mRoot = r;
+        } else if (p.mRight == n) {
+            p.mRight = r;
+        } else {
+            p.mLeft = r;
+        }
 
-		if (p == null) { // n is root
-			mRoot = r;
-		}
-		else if (p.mRight == n) {
-			p.mRight = r;
-		}
-		else {
-			p.mLeft = r;
-		}
-
-		r.mParent = p;
-	}
+        r.mParent = p;
+    }
 
 
-	// This method is used by insert. It is complete.
-	// Inserts the key/value into the BST, and returns true if the key wasn't previously in the tree.
-	private boolean bstInsert(Node newNode, Node currentNode) {
-		if (mRoot == null) {
-			// case 1
-			mRoot = newNode;
-			return true;
-		} 
-		else {
-			int compare = currentNode.mKey.compareTo(newNode.mKey);
-			if (compare < 0) {
-				// newNode is larger; go right.
-				if (currentNode.mRight != NIL_NODE) {
-					return bstInsert(newNode, currentNode.mRight);
-				}
-				else {
-					currentNode.mRight = newNode;
-					newNode.mParent = currentNode;
-					mCount++;
-					return true;
-				}
-			} 
-			else if (compare > 0) {
-				if (currentNode.mLeft != NIL_NODE) {
-					return bstInsert(newNode, currentNode.mLeft);
-				}
-				else {
-					currentNode.mLeft = newNode;
-					newNode.mParent = currentNode;
-					mCount++;
-					return true;
-				}
-			} 
-			else {
-				// found a node with the given key; update value.
-				currentNode.mValue = newNode.mValue;
-				return false; // did NOT insert a new node.
-			}
-		}
-	}
+    // This method is used by insert. It is complete.
+    // Inserts the key/value into the BST, and returns true if the key wasn't previously in the tree.
+    private boolean bstInsert(Node newNode, Node currentNode) {
+        if (mRoot == null) {
+            // case 1
+            mRoot = newNode;
+            return true;
+        } else {
+            int compare = currentNode.mKey.compareTo(newNode.mKey);
+            if (compare < 0) {
+                // newNode is larger; go right.
+                if (currentNode.mRight != NIL_NODE) {
+                    return bstInsert(newNode, currentNode.mRight);
+                } else {
+                    currentNode.mRight = newNode;
+                    newNode.mParent = currentNode;
+                    mCount++;
+                    return true;
+                }
+            } else if (compare > 0) {
+                if (currentNode.mLeft != NIL_NODE) {
+                    return bstInsert(newNode, currentNode.mLeft);
+                } else {
+                    currentNode.mLeft = newNode;
+                    newNode.mParent = currentNode;
+                    mCount++;
+                    return true;
+                }
+            } else {
+                // found a node with the given key; update value.
+                currentNode.mValue = newNode.mValue;
+                return false; // did NOT insert a new node.
+            }
+        }
+    }
 
 
-	public Node getmRoot(){
-		return mRoot;
-	}
+    public Node getmRoot() {
+        return mRoot;
+    }
 }
--- src/lab4/lab4_main.java
+++ src/lab4/lab4_main.java
@@ -4,23 +4,50 @@ import java.io.*;
 import java.util.*;
 
 public class lab4_main {
-    public static void main(String[]arg) throws FileNotFoundException {
+    public static void main(String[] arg) throws FileNotFoundException {
         File homeruns = new File("players_homeruns.csv");
         Scanner fileScanner = new Scanner(homeruns);
         RedBlackTreeMap<String, Integer> players = new RedBlackTreeMap<String, Integer>();
 
+        // first 5
         int ctr = 0;
-        while (fileScanner.hasNextLine() && ctr < 5){
-            String [] tempArray = fileScanner.nextLine().split(",");
+        while (fileScanner.hasNextLine() && ctr < 5) {
+            String[] tempArray = fileScanner.nextLine().split(",");
             int tempHomeruns = Integer.parseInt(tempArray[1]);
             players.add(tempArray[0], tempHomeruns);
-            System.out.printf("Adding %s : %d\n", tempArray[0], tempHomeruns);
             ctr++;
         }
 
-        System.out.println("\nPrint structure using pre-order traversal\n");
+        System.out.println("Print structure using pre-order traversal for first 5 names:");
         players.printStructure(players.getmRoot());
 
-        System.out.println(players.find("Barry Bonds"));
+        // next 5
+        while (fileScanner.hasNextLine() && ctr < 10) {
+            String[] tempArray = fileScanner.nextLine().split(",");
+            int tempHomeruns = Integer.parseInt(tempArray[1]);
+            players.add(tempArray[0], tempHomeruns);
+            ctr++;
+        }
+
+        System.out.println("\nPrint structure using pre-order traversal for 10 added names total:");
+        players.printStructure(players.getmRoot());
+
+        System.out.printf("\na) Finding key that is a leaf (Willie Mays): %d", players.find("Willie Mays"));
+        System.out.printf("\nb) Finding key that is a root (Honus Wagner): %d", players.find("Honus Wagner"));
+        System.out.printf("\nc) Finding key that has one NIL child and one non-NIL child (Rogers Hornsby): %d", players.find("Rogers Hornsby"));
+        System.out.printf("\nd) Finding key that is in a red node (Ted Williams): %d\n", players.find("Ted Williams"));
+
+        System.out.println("\nAdding the rest of the names to the red black tree...");
+        while (fileScanner.hasNextLine()) {
+            String[] tempArray = fileScanner.nextLine().split(",");
+            int tempHomeruns = Integer.parseInt(tempArray[1]);
+            players.add(tempArray[0], tempHomeruns);
+            ctr++;
+        }
+
+        System.out.printf("\na) Finding key that is a leaf (Willie Mays): %d", players.find("Willie Mays"));
+        System.out.printf("\nb) Finding key that is a root (Honus Wagner): %d", players.find("Honus Wagner"));
+        System.out.printf("\nc) Finding key that has one NIL child and one non-NIL child (Rogers Hornsby): %d", players.find("Rogers Hornsby"));
+        System.out.printf("\nd) Finding key that is in a red node (Ted Williams): %d", players.find("Ted Williams"));
     }
 }
